5d345e09ab40b03d1d927600232a9baab1af15dd
adriankristanto <49110670+adriankristanto@users.noreply.github.com>
2022-01-18 19:00:10 +1100

    added a working reset game feature

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,13 +1,10 @@
 /**
  * Reference: https://kyleshevlin.com/tic-tac-toe
  */
-import React, { useState } from "react";
+import React from "react";
 import Game from "./Game";
-import { Modal, Button } from "react-bootstrap";
 
 function App() {
-    const [modalOpen, setModalOpen] = useState(false);
-    const [modalMessage, setModalMessage] = useState("");
     const gameSettings = {
         boardSize: 3,
         players: ["X", "O"],
@@ -21,37 +18,9 @@ function App() {
         justifyContent: "center",
     };
 
-    const handleGameOver = (gameOverState) => {
-        const { isDraw, winner } = gameOverState;
-
-        if (isDraw) {
-            setModalMessage("Draw!");
-            setModalOpen(true);
-            return;
-        }
-
-        if (winner) {
-            setModalMessage(`${winner} wins!`);
-            setModalOpen(true);
-            return;
-        }
-    };
-
-    const handleGameReset = () => {};
-
     return (
         <div className="App" style={appStyle}>
-            <Game gameSettings={gameSettings} onGameOver={handleGameOver} />
-            <Modal show={modalOpen} backdrop="static" keyboard={false} centered>
-                <Modal.Body className="border-0 d-flex align-items-center justify-content-center p-5">
-                    <h1 className="m-5 p-5">{modalMessage}</h1>
-                </Modal.Body>
-                <Modal.Footer className="border-0">
-                    <Button variant="secondary" onClick={handleGameReset}>
-                        Reset Game
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            <Game gameSettings={gameSettings} />
         </div>
     );
 }
--- app/src/components/Game.js
+++ app/src/components/Game.js
@@ -5,21 +5,25 @@ import {
     checkForDraw,
     checkForWinner,
 } from "../utils/game";
+import { Modal, Button } from "react-bootstrap";
 
 /**
  * component responsible for managing the game state,
  * e.g. what happens when the user clicks on a cell,
  * runs checks on whether the game has completed, etc.
+ * This also includes showing UI components after the game is over.
  *
  * @param {*} gameSettings an object containing boardSize and players settings
  * @param {*} onGameOver a callback that is called when the game ends, either because of a draw or there is a winner
  *                       it needs to accept the game over state, containing isDraw boolean and winner player symbol
  */
-export default function Game({ gameSettings, onGameOver }) {
+export default function Game({ gameSettings }) {
     const [boardState, setBoardState] = useState(
         generateInitialBoardState(gameSettings.boardSize)
     );
     const [turn, setTurn] = useState(0);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalMessage, setModalMessage] = useState("");
 
     useEffect(() => {
         const isDraw = checkForDraw(boardState);
@@ -27,7 +31,8 @@ export default function Game({ gameSettings, onGameOver }) {
         if (isDraw || winner) {
             onGameOver({ isDraw, winner });
         }
-    }, [boardState, onGameOver]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [boardState]);
 
     const handleClick = (x, y) => () => {
         // Reference: https://dev.to/samanthaming/how-to-deep-clone-an-array-in-javascript-3cig
@@ -43,6 +48,36 @@ export default function Game({ gameSettings, onGameOver }) {
         }
     };
 
+    const onGameOver = (gameOverState) => {
+        const { isDraw, winner } = gameOverState;
+
+        if (isDraw) {
+            setModalMessage("Draw!");
+            openModal();
+            return;
+        }
+
+        if (winner) {
+            setModalMessage(`${winner} wins!`);
+            openModal();
+            return;
+        }
+    };
+
+    const handleGameReset = () => {
+        setModalMessage("");
+        setBoardState(generateInitialBoardState(gameSettings.boardSize));
+        closeModal();
+    };
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
     return (
         <div>
             <Board
@@ -50,6 +85,16 @@ export default function Game({ gameSettings, onGameOver }) {
                 handleClick={handleClick}
                 boardState={boardState}
             />
+            <Modal show={modalOpen} backdrop="static" keyboard={false} centered>
+                <Modal.Body className="border-0 d-flex align-items-center justify-content-center p-5">
+                    <h1 className="m-5 p-5">{modalMessage}</h1>
+                </Modal.Body>
+                <Modal.Footer className="border-0">
+                    <Button variant="secondary" onClick={handleGameReset}>
+                        Reset Game
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
